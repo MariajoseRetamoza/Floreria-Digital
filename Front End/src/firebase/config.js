f4c425fa7a6c1262900687a53fcf30d716760509
chamistik <chamistik33@gmail.com>
2025-05-26 11:55:05 -0700

    Eliminacion de la api key

--- Front End/src/firebase/config.js
+++ Front End/src/firebase/config.js
@@ -5,7 +5,6 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHQtXCSyvhB93EShkK6jxKZXRc-ZY0ASk",
   authDomain: "floreria-digital.firebaseapp.com",
   projectId: "floreria-digital",
   storageBucket: "floreria-digital.firebasestorage.app",
