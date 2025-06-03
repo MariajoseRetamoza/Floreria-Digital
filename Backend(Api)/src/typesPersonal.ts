b7f1ef6a63d9a6c216c876d9b6dee76ab021cce0
Mariajose Retamoza <162388837+MariajoseRetamoza@users.noreply.github.com>
2025-06-02 22:48:42 -0700

    deldete

--- Backend(Api)/src/typesPersonal.ts
+++
@@ -1,9 +0,0 @@
-export interface Personal {
-  id: number;
-  nombre: string;
-  direccion: string;
-  telefono: string;
-  estatus: number;
-}
-
-export type PersonalNuevo = Omit<Personal, "id">;
--- Front End/src/modulos/pedidos/api/pedidosAPI.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const pedidosApi = axios.create({
-  baseURL: "http://localhost:3002/api/pedidos",
-});
-
-export default pedidosApi;
