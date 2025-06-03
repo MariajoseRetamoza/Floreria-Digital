8848d9ab723863eeaf2804f294c176ded795f174
Mariajose Retamoza <162388837+MariajoseRetamoza@users.noreply.github.com>
2025-06-03 12:31:32 -0700

    Visual

--- Frontend_cliente_tienda/src/main.ts
+++ Frontend_cliente_tienda/src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue';
 import router from './router';
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
+import './assets/global.css';
 
 createApp(App).use(router).use(Toast).mount('#app');
