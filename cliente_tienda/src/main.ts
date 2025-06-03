778b43bca6cc2b79f42d3bee63adbb5f3839217f
Mariajose Retamoza <162388837+MariajoseRetamoza@users.noreply.github.com>
2025-06-03 16:34:47 -0700

    Importe

--- cliente_tienda/src/main.ts
+++ cliente_tienda/src/main.ts
@@ -4,5 +4,7 @@ import router from './router';
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 import './assets/global.css';
+import './assets/base.css'
+
 
 createApp(App).use(router).use(Toast).mount('#app');
