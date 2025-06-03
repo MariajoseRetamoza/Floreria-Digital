081a0ed3e49e940ae079bfd60a1917386693975f
Mariajose Retamoza <162388837+MariajoseRetamoza@users.noreply.github.com>
2025-06-03 13:25:26 -0700

    Eliminacion del front

--- Frontend_cliente_tienda/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Navbar />
-  <router-view />
-</template>
-
-<script setup lang="ts">
-import Navbar from './components/Navbar.vue';
-</script>
--- Frontend_cliente_tienda/src/utils/validators.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const personalSchema = z.object({
-  nombre: z.string().min(1, 'El nombre es requerido'),
-  direccion: z.string().min(1, 'La dirección es requerida'),
-  telefono: z.string().min(10, 'El teléfono es inválido'),
-  estatus: z.enum(['1', '2'])
-});
